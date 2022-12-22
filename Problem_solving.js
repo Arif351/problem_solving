6840840a7c9397fae14571920beca363d55bddb5
Arif351 <ariful8836@gmail.com>
2022-12-23 00:57:10 +0600

    vowel counted

--- Problem_solving.js
+++ Problem_solving.js
@@ -1,5 +1,5 @@
 
-// Leap Year
+// 1. Leap Year
 const leapYear = (year) => {
     if ((year % 400 === 0 || year % 4 === 0) && (year % 100 !== 0)) {
         console.log(`${year} is Leap year`);
@@ -10,10 +10,20 @@ const leapYear = (year) => {
 }
 leapYear(2020)
 
-// Vowel
+// 2. Counting Vowel
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 
-
-
+const countVowels = sentences => {
+    let count = 0;
+    const letters = Array.from(sentences);
+    letters.forEach(letter => {
+        if (vowels.includes(letter)) {
+            count++;
+        }
+    });
+    return count;
+}
+console.log(countVowels("Hi, I am Ariful Islam, a Full Stack Web Developer. I love to work with React js, Javascript, MongoDB, Express js, Node, Tailwind CSS, Bootstrap, React Bootstrap, and modern HTML. I have already done some projects about services, teaching, review, etc. I love to explore new technologies and work with them. I can work under pressure."));
 
 
 
